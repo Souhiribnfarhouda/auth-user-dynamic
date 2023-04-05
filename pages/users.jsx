cecdf2e1ecbdc9b91c24e2b0bff45121581ab244
Ibn Farhouda Souhir <farhoudasouhir@gmail.com>
2023-04-05 14:38:12 +0100

    first commit

---
+++ pages/users.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {useSession,signIn, signOut} from 'next-auth/react'
+
+const users = () => {
+    const {data: session} = useSession();
+    console.log(session);
+     if (session) {
+        return (
+            <div>
+                <p>welcome  {session.user.email}</p> 
+                <button onClick={() => signOut()}>Sign out</button>
+            </div>
+        );
+      }else {
+      return (
+        <div>
+            <p>You are not signed in. </p>
+            <button onClick={() => signIn()}>Sign In</button>
+        </div>
+        );
+      }
+
+};
+export default users
+     
